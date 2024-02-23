9ca7ca9dc1ebe0e4e34398e783a4a56092da915b
EmpireAntz <thebigboognish@gmail.com>
2024-02-22 21:13:27 -0800

    configured boilerplate

---
+++ Main/client/src/main.jsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
