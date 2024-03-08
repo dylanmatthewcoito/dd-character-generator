a696911859a017fa76d613521a2455b30532ab46
chris <davischris25139@yahoo.com>
2024-03-07 22:24:33 -0800

    fixed merge conflicts and completed userAuth

--- Main/client/src/components/Navbar.jsx
+++ Main/client/src/components/Navbar.jsx
@@ -19,29 +19,36 @@ const Navbar = () => {
   };
 
   return (
-  <div>
-    {!isAuthPage && ( // Only render the navigation links if NOT on the AuthPage
-    <nav className="navbar py-4 fixed-top">
-      <div className="container">
-        <div className="logo red">D&D Character Generator</div>
-          <ul className="navbar-nav ml-auto">
-            <li className="">
-              <Link className="home red-link" to="/app/prompt">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="donate-link red-link" to="/app/donate">Donate</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="profile red-link" to="/app/profile">Characters</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="log-out red-link" to="/" onClick={handleLogout}>Log Out</Link>
-            </li>
-          </ul>
-      </div>
-    </nav>
-    )}
-  </div>
+
+    <div>
+      {!isAuthPage && ( // Only render the navigation links if NOT on the AuthPage
+        <nav className="navbar py-4 fixed-top">
+          <div className="container">
+            <div className="logo red">D&D Character Generator</div>
+            <div className="{`hamburger ${isOpen ? 'open' : ''}`}, " onClick={toggleMenu}>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+            <ul className={`navbar-nav ml-auto ${isOpen ? 'open' : ''}`}>
+              <li className="nav-item">
+                <Link className="home red-link" to="/app/prompt" onClick={toggleMenu}>Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="donate-link red-link" to="/app/donate" onClick={toggleMenu}>Donate</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="profile red-link" to="/app/profile" onClick={toggleMenu}>My Characters</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="log-out red-link" to="/" onClick={handleLogout}>Log Out</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      )}
+    </div>
+
   );
 };
 
